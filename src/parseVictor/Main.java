9a822cdd1b60fdf408b928427e888f6492f2da76
Albert Balbastre-Morte <bulbastre@gmail.com>
2017-11-17 19:41:16 +0100

    FIRST COMMIT

---
+++ src/parseVictor/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package parseVictor;
+
+/**
+ * @author iaw46994355
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
